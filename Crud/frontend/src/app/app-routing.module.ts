fb2c3fffa36d5c5b4bd40bf90594ff8f18428675
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-23 07:13:21 -0300

    Criação de rotas para o menu

--- Crud/frontend/src/app/app-routing.module.ts
+++ Crud/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './views/home/home.component'; //.ts
+import {ProductCrudComponent} from './views/product-crud/product-crud.component'; //.ts
+
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }, 
+  {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
