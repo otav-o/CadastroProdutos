7c44452da249313cbc97f64a3edc9cad9b4eb3cb
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-24 15:16:03 -0300

    Criar método post com HttpClientModule

--- Crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ Crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
+import {Product} from '../product.model';
 
 @Component ({
     selector: 'app-product-create',
@@ -9,13 +10,21 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+    product: Product = {
+        name: 'Produto de Teste',
+        price: 125.98
+    }
+
     constructor(private productService: ProductService, private router: Router) { }
 
     ngOnInit(): void {  
     }
 
     createProduct() : void {
-        this.productService.showMessage('Operação bem sucedida!');
+        this.productService.create(this.product).subscribe(() => {
+            this.productService.showMessage('Operação bem sucedida!');
+            this.router.navigate(['/products']);
+        }); // subscribe recebe uma função que será executada quando ocorrer o evento.
     }
 
     cancel() : void {
--- Crud/frontend/src/app/components/product/product.service.ts
+++ Crud/frontend/src/app/components/product/product.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import {Product} from './product.model'
 import { Injectable } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root' // significa que o service é singleton (uma única instância). Atributos compartilhados!
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  baseUrl = 'http://localhost:3001/products';
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     console.log(msg);
@@ -16,6 +21,9 @@ export class ProductService {
       horizontalPosition: 'right',
       verticalPosition: 'top'
     });
+  }
 
+  create (product: Product): Observable<Product> {
+      return this.http.post<Product>(this.baseUrl, product); // post retorna um Observable
   }
 }
