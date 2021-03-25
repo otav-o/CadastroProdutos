7716c5dce2a3b52f83d993072b1a69503dccd293
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-25 11:09:35 -0300

    Criar a própria tabela de exibição em product-read.component
    Importação do padrão monetário brasileiro. Uso de property binding para recuperar products em dataSource

--- Crud/frontend/src/app/components/product/product-read/product-read.component.ts
+++ Crud/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -9,7 +9,8 @@ import { ProductService } from '../product.service';
 })
 export class ProductReadComponent implements OnInit {
 
-  products: Product[] | undefined
+  products: Product[] = []
+  displayedColumns = ['id', 'name', 'preco']
 
   constructor(private productService: ProductService) { }
 
