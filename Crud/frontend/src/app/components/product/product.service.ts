ce65cb727af2e79ad49e19f44804ac98edad0488
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-26 18:40:05 -0300

    Adicionar mensagem de erro quando há problema no backend
    Também modificar a cor da mensagem de sucesso pela classe CSS .msg-success

--- Crud/frontend/src/app/components/product/product.service.ts
+++ Crud/frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import {Product} from './product.model'
 import { Injectable } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root' // significa que o service é singleton (uma única instância). Atributos compartilhados!
@@ -13,36 +14,56 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     console.log(msg);
 
     this.snackBar.open(msg, 'fechar', { //mensagem, action, objeto de configuração
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError?['msg-error']:['msg-success'] // classes css que podem ser aplicadas
     });
   }
 
   create (product: Product): Observable<Product> {
-      return this.http.post<Product>(this.baseUrl, product); // post retorna um Observable
+      return this.http.post<Product>(this.baseUrl, product).pipe( // post retorna um Observable
+        map(obj => obj), // retornar o objeto sem fazer nada, pois o objetivo é capturar o erro
+        catchError(e => this.errorHandler(e)) // arrow function somente para garantir que this é a instância atual
+      );
   }
 
   read(): Observable<Product[]> {
-	  return this.http.get<Product[]>(this.baseUrl);
+	  return this.http.get<Product[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   readById(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      catchError(e => this.errorHandler)
+    );
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      catchError(e => this.errorHandler)
+    );
   }
 
   delete(id: number) : Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      catchError(e => this.errorHandler)
+    );
   }
+  
+  errorHandler(e: any): Observable<any> {
+    console.log(e);
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY; // retornar observable vazio
+  }
+
 }
